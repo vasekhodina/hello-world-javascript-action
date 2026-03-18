e2c4cd1cfd33b1812aa4753bb93ed687e8091ddb
Vaclav Hodina <Vaclav.Hodina@ysoft.com>
2026-03-18 15:53:55 +0100

    Update workflow action again

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,9 +4,8 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 const config = {
   input: "src/index.js",
   output: {
-    esModule: true,
     file: "dist/index.js",
-    format: "es",
+    format: "cjs",
     sourcemap: true,
   },
   plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
